78f89bbc30b41088c81e8eece78ad0720a33327e
dev-udit09 <udit@technoproject.in>
2023-03-15 15:21:09 +0530

    admin credentials

--- controller/img.controller.js
+++ controller/img.controller.js
@@ -1,7 +1,7 @@
 const db = require("../models");
 const imgUpload = db.images;
 const multer = require("multer");
-const md5 = require('md5');
+const md5 = require("md5");
 const path = require("path");
 // const upload = multer({ storage: fileStorageEngine });
 
@@ -52,46 +52,45 @@ const path = require("path");
 //   });
 // };
 exports.uploadImg = async (req, res) => {
+  const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./public/images");
+    },
+    filename: function (req, file, callback) {
+      callback(null, md5(Date.now()) + path.extname(file.originalname));
+    },
+  });
 
-    const storage = multer.diskStorage({
-        destination: function (req, file, cb) {
-          cb(null, "./images");
-        },
-        filename: function (req, file, callback) {
-          callback(null, md5(Date.now()) + path.extname(file.originalname));
-        },
+  const uploaFile = multer({
+    storage: storage,
+  }).single("image");
+
+  uploaFile(req, res, async (err) => {
+    if (!req.file) {
+      res.status(500).send({
+        success: false,
+        data: [],
+        message: "Select Image",
       });
-    
-      const uploaFile = multer({
-        storage: storage,
-      }).single("image");
-    
-      uploaFile(req, res, async (err) => {
-        if (!req.file) {
-            res.status(500).send({
-                success: false,
-                data: [],
-                message: "Select Image",
-            });
-        } else if (err) {
-            res.status(500).send({
-                success: false,
-                data: [],
-                message: "not upload",
-            });
-        } else {
-            imgUpload.create({ pic:process.env.MAIN_URL + req.file.filename}).then((data) => {
-                res.status(200).send({
-                    success: true,
-                    data: {
-                        filepath_url: req.file.filename,
-                        url: process.env.MAIN_URL + req.file.filename,
-                    },
-                    message: "Image Stored in databse!",
-                });
-            }).catch((err) => {
-                res.send(err);
-            });
-        }
-    })
-  }
+    } else if (err) {
+      res.status(500).send({
+        success: false,
+        data: [],
+        message: "not upload",
+      });
+    } else {
+      // imgUpload.create({ pic:process.env.MAIN_URL + req.file.filename}).then((data) => {
+      res.status(200).send({
+        success: true,
+        data: {
+          filepath_url: req.file.filename,
+          url: process.env.MAIN_URL + "images/" + req.file.filename,
+        },
+        message: "Image Stored in databse!",
+      });
+      // }).catch((err) => {
+      //     res.send(err);
+      // });
+    }
+  });
+};
